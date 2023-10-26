91493a5fb9fd65a264d79a441df8c0883e627a0e
sundengyu <dengyu.sun@iomesh.com>
2023-10-26 13:53:26 +0800

    not align

--- module/zfs/zio.c
+++ module/zfs/zio.c
@@ -194,7 +194,13 @@ zio_init(void)
 		 * Since userland probably doesn't need optimized buffer caches,
 		 * we just force 4K alignment on everything.
 		 */
-		align = 8 * SPA_MINBLOCKSIZE;
+		// align = 8 * SPA_MINBLOCKSIZE;
+
+		if (size < PAGESIZE) {
+			align = SPA_MINBLOCKSIZE;
+		} else if (IS_P2ALIGNED(size, p2 >> 2)) {
+			align = PAGESIZE;
+		}
 #else
 		if (size < PAGESIZE) {
 			align = SPA_MINBLOCKSIZE;
